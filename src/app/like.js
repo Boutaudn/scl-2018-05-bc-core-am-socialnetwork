421f763000118a3c34919fb784440bcfb0c414ae
Kaybel <kblmrtnz@gmail.com>
2018-07-12 12:55:06 -0400

    pagina de registro aparece y desaparece, terminos y condiciones, se aplica un poco bootstrap

--- src/app/like.js
+++ src/app/like.js
@@ -3,3 +3,23 @@ function clickCounter() {
     counter += 1;
     document.getElementById("icon").innerHTML = counter;
 }
+
+function registroYes() {
+  document.getElementById('registro').classList.remove('d-none');
+}
+function registroNone() {
+  document.getElementById('registro').classList.add('d-none');
+}
+function registroPage() {
+  if (registro.className.indexOf('d-none') >= 0) {
+    registroYes();
+  }
+}
+function registrolist() {
+  if (registro.className.indexOf('d-none') >= 0){
+    registroNone()
+  }
+}
+function termsAndConditions() {
+  alert('Los terminos y condiciones son blablablablabla');
+}
