2d0709d96fa5ec5b97b4e384936f3dc75c269457
Boutaudn <boutaudn@gmail.com>
2018-07-18 09:07:26 -0400

    inicializando firebase

---
+++ src/assets/js/app.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyA9KOG2Mc1NSFQ7h9OPNJbNJEDxJ62ivYI",
+  authDomain: "red-social-cfa45.firebaseapp.com",
+  projectId: "red-social-cfa45"
+});
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
