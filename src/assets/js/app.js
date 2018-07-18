5b354fc445c547292e6ebeb30c3b06f3bf216ab8
Kaybel <kblmrtnz@gmail.com>
2018-07-18 09:55:45 -0400

    eliminando archivos que ya no son de utilidad

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,34 +5,62 @@ firebase.initializeApp({
   projectId: "red-social-cfa45"
 });
 
-// Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
+// funcion para el contador de "aportes"
+let counter = 0;
+function clickCounter() {
+  counter += 1;
+  document.getElementById("icon").innerHTML = counter;
+  document.getElementById('registro').classList.add('d-none');
+}
 
-// Subir un post
+// alert de terminos y condiciones
+function termsAndConditions() {
+  alert('Los terminos y condiciones son blablablablabla');
+  confirm('Los terminos y condiciones son blablablablabla');
+}
 
-// Agregar documento
-function guardar() {
-  var nombre = document.getElementById('nombre').value;
-  var apellido = document.getElementById('apellido').value;
-  var fecha = document.getElementById('fecha').value;
-  let d = new Date();
-  let time = d.getHours() + ":" + d.getMinutes();
-  let date = d.getDate() + "." + d.getMonth() + "." + d.getFullYear();
+//evento enviar msj
+function sendMessage() {//pensar qué debe ocurrir para enviar un mensaje, desde el usuario
+  const currentUser = firebase.auth().currentUser;
+  const messageText = document.getElementById('inputMessageChat').value;
 
-  db.collection("users").add({
-    first: nombre,
-    last: apellido,
-    born: fecha,
-    date: date,
-    time: time
-  })
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-      document.getElementById('nombre').value = '';
-      document.getElementById('apellido').value = '';
-      document.getElementById('fecha').value = '';
-    })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
+  document.getElementById('message-box').value = "";
+
+  //para tener una nueva llave en la colección messages
+  const newMessageKey = firebase.database().ref().child('messages').push().key; //metodo de firebase
+
+  firebase.database().ref(`messages/${newMessageKey}`).set({
+    creator: currentUser.uid,
+    creatorName: currentUser.displayName,
+    text: messageText,
+    //profile_picture: imgurl
+  }); //ref, ruta para guardar los mensajes
+}
+
+// evento mensaje
+function setupMessages() {
+  firebase.database().ref('messages')
+    .limitToLast(100)
+    .on('child_added', (newMessage) => {
+      document.getElementById("message-container").innerHTML +=
+        `<li class="right clearfix">
+                          <div class="chat-body clearfix">
+                              <div class="header">
+                                  <small class=" text-muted">
+                                      <span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                  <strong class="pull-right primary-font">${newMessage.val().creatorName}</strong>
+                              </div>
+                              <p>${newMessage.val().text}</p>
+                          </div>
+                      </li>`;
     });
 }
+
+// evento enviar desde el teclado con boton enter
+let inputMessage = document.getElementById("inputMessageChat");
+inputMessage.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("btn-chat").click();
+  }
+});
