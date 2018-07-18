8111b833978c3ec111e7c45989e33d0d2fd061e2
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-18 18:58:12 -0400

    acomodado con logout

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,3 @@
-
 window.onload = () => {
   firebase.auth().onAuthStateChanged((user) => {
       if (user) {             //Si estamos logueados
@@ -109,64 +108,62 @@ firebase.initializeApp({
   projectId: "red-social-cfa45"
 });
 
-// Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
+// funcion para el contador de "aportes"
+let counter = 0;
+function clickCounter() {
+  counter += 1;
+  document.getElementById("icon").innerHTML = counter;
+  document.getElementById('registro').classList.add('d-none');
+}
 
+// alert de terminos y condiciones
+function termsAndConditions() {
+  alert('Los terminos y condiciones son blablablablabla');
+  confirm('Los terminos y condiciones son blablablablabla');
+}
 
-// Subir un post
+//evento enviar msj
+function sendMessage() {//pensar qué debe ocurrir para enviar un mensaje, desde el usuario
+  const currentUser = firebase.auth().currentUser;
+  const messageText = document.getElementById('inputMessageChat').value;
 
-// Agregar documento
-function guardar() {
-  let texto = document.getElementById('text').value;
-  let name = "mi@mail.com";
-  let d = new Date();
-  let time = d.getHours() + ":" + d.getMinutes();
-  let date = d.getDate() + "." + d.getMonth() + "." + d.getFullYear();
-  let title = document.getElementById('titulo').value;
+  document.getElementById('message-box').value = "";
 
-  db.collection("post").add({
-    name: name,
-    title: title,
-    texto: texto,
-    date: date,
-    time: time
-  })
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-      console.log("text");
-      document.getElementById('text').value = '';
-    })
-    .catch(function (error) {
-      console.error("Error adding document: ", error);
+  //para tener una nueva llave en la colección messages
+  const newMessageKey = firebase.database().ref().child('messages').push().key; //metodo de firebase
+
+  firebase.database().ref(`messages/${newMessageKey}`).set({
+    creator: currentUser.uid,
+    creatorName: currentUser.displayName,
+    text: messageText,
+    //profile_picture: imgurl
+  }); //ref, ruta para guardar los mensajes
+}
+
+// evento mensaje
+function setupMessages() {
+  firebase.database().ref('messages')
+    .limitToLast(100)
+    .on('child_added', (newMessage) => {
+      document.getElementById("message-container").innerHTML +=
+        `<li class="right clearfix">
+                          <div class="chat-body clearfix">
+                              <div class="header">
+                                  <small class=" text-muted">
+                                      <span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                  <strong class="pull-right primary-font">${newMessage.val().creatorName}</strong>
+                              </div>
+                              <p>${newMessage.val().text}</p>
+                          </div>
+                      </li>`;
     });
 }
 
-// Leer comentarios
-var posteos = document.getElementById('muro');
-
-db.collection("post").onSnapshot((querySnapshot) => {
-  posteos.innerHTML = '';
-  querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-      posteos.innerHTML += `
-      <div id="post">
-      <div class="card" style="width: 20rem;">
-        <div class="card-body">
-          <h5>${doc.data().title}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${doc.data().name}</h6>
-          <p>${doc.data().texto}</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>`
-      /* <tr>
-            <th scope="row">${doc.data().time}</th>
-            <td>${doc.data().first}</td>
-            <td>${doc.data().last}</td>
-            <td>${doc.data().born}</td>
-            <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
-            <td><button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().first}', '${doc.data().last}', '${doc.data().born}')">Editar</button></td>
-          </tr> */
-
-    });
+// evento enviar desde el teclado con boton enter
+let inputMessage = document.getElementById("inputMessageChat");
+inputMessage.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("btn-chat").click();
+  }
 });
