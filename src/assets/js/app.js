6b2c2f7d97e6c64f5d7bc1e38da2ac78a84ac54e
Boutaudn <boutaudn@gmail.com>
2018-07-18 10:30:55 -0400

    cambios de chat

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,31 +8,104 @@ firebase.initializeApp({
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
+
 // Subir un post
 
 // Agregar documento
 function guardar() {
-  var nombre = document.getElementById('nombre').value;
-  var apellido = document.getElementById('apellido').value;
-  var fecha = document.getElementById('fecha').value;
+  var texto = document.getElementById('text').value;
+  let name = "mi@mail.com";
   let d = new Date();
   let time = d.getHours() + ":" + d.getMinutes();
   let date = d.getDate() + "." + d.getMonth() + "." + d.getFullYear();
+  let title = document.getElementById('titulo').value;
 
-  db.collection("users").add({
-    first: nombre,
-    last: apellido,
-    born: fecha,
+  db.collection("post").add({
+    name: name,
+    title: title,
+    texto: texto,
     date: date,
     time: time
   })
     .then(function (docRef) {
       console.log("Document written with ID: ", docRef.id);
-      document.getElementById('nombre').value = '';
-      document.getElementById('apellido').value = '';
-      document.getElementById('fecha').value = '';
+      console.log("text");
+      document.getElementById('text').value = '';
     })
     .catch(function (error) {
       console.error("Error adding document: ", error);
     });
+}
+
+// Leer comentarios
+var posteos = document.getElementById('muro');
+
+db.collection("post").onSnapshot((querySnapshot) => {
+  posteos.innerHTML = '';
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+      posteos.innerHTML += `
+      <div id="post">
+      <div class="card" style="width: 20rem;">
+        <div class="card-body">
+          <h5>${doc.data().title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${doc.data().name}</h6>
+          <p>${doc.data().texto}</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>`
+      /* <tr>
+            <th scope="row">${doc.data().time}</th>
+            <td>${doc.data().first}</td>
+            <td>${doc.data().last}</td>
+            <td>${doc.data().born}</td>
+            <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
+            <td><button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().first}', '${doc.data().last}', '${doc.data().born}')">Editar</button></td>
+          </tr> */
+
+    });
+});
+
+// Borrar documentos
+function eliminar(id){
+  db.collection("users").doc(id).delete().then(function() {
+    console.log("Document successfully deleted!");
+  }).catch(function(error) {
+    console.error("Error removing document: ", error);
+  });
+}
+
+// Editar documento
+function editar(id, nombre, apellido, fecha){
+  document.getElementById('nombre').value = nombre;
+  document.getElementById('apellido').value = apellido;
+  document.getElementById('fecha').value = fecha;
+  var boton = document.getElementById('boton');
+  boton.innerHTML = 'Editar';
+  boton.onclick = function(){
+
+
+    var washingtonRef = db.collection("users").doc(id);
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var fecha = document.getElementById('fecha').value;
+
+    return washingtonRef.update({
+      first: nombre,
+      last: apellido,
+      born: fecha,
+    })
+    .then(function() {
+      console.log("Document successfully updated!");
+      boton.innerHTML = 'Guardar';
+      document.getElementById('nombre').value = '';
+      document.getElementById('apellido').value = '';
+      document.getElementById('fecha').value = '';
+    })
+    .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+    });
+  }
 }
