60f067303aa59fb62d83364851ab9dda929c3db4
Boutaudn <boutaudn@gmail.com>
2018-07-19 08:29:55 -0400

    arreglando la embarrada del merge de ayer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,109 +1,5 @@
-
-window.onload = () => {
-  firebase.auth().onAuthStateChanged((user) => {
-      if (user) {             //Si estamos logueados
-
-         showDashboardPage(); //Cambiar a la pagina de dasboard
-         setupMessages();     //Dejar todo listo para mostrar mensajes. 
-
-      } else {                //No estamos logueados
-         showLoginPage();     //Cambiar a la pagina de login
-      }
-  });
-};
-
-/* ----Login---- */
-function login() {
-  const emailValue = document.getElementById("login-username").value;
-  const passwordValue = document.getElementById("login-password").value;
-  firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
-      .then(() => {
-          console.log("Usuario con login exitoso");
-      })
-      .catch((error) => {
-          console.log("Error de firebase > " + error.code);
-          console.log("Error de firebase, mensaje > " + error.message);
-      });
-}
-
-function loginWithFacebook() {
-  const provider = new firebase.auth.FacebookAuthProvider();
-  //provider.addScope("user_birthday"); tienen que pedirle permiso a facebook
-  provider.setCustomParameters({
-      'display': 'popup'
-  });
-  firebase.auth().signInWithPopup(provider)
-      .then(() => {
-          console.log("Login con facebook");
-      })
-      .catch((error) => {
-          console.log("Error de firebase > " + error.code);
-          console.log("Error de firebase, mensaje > " + error.message);
-      });
-  scope: 'email'//acceder al email del usuario
-
-}
-
-function logout() {
-  firebase.auth().signOut()
-      .then(() => {
-          console.log("Chao");
-      })
-      .catch();
-}
-/* -------FIN LOGIN------- */
-
-/* -------router, login, register------ */
-function showPerfilPage() {
-  $('#login-page').hide(); 
-  $('#perfil-page').show(); 
-  $('#dashboard-page').hide();
-  $('#register-page').hide();
-}
-
-function showRegisterPage() {
-  $('#login-page').hide(); 
-  $('#perfil-page').hide(); 
-  $('#dashboard-page').hide();
-  $('#register-page').show();
-}
-
-function showDashboardPage() {
-  $('#login-page').hide(); 
-  $('#perfil-page').hide(); 
-  $('#dashboard-page').show();
-  $('#register-page').hide();
-}
-
-function showLoginPage() {
-  $('#login-page').show(); 
-  $('#perfil-page').hide(); 
-  $('#dashboard-page').hide();
-  $('#register-page').hide();
-}
-/* -------fin router------ */
-
-/* --------Register------- */
-function register() {
-  const emailValue = document.getElementById("register-email").value;
-  const passwordValue = document.getElementById("register-password").value;
-  
-  firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
-      .then(() => {
-          console.log("Usuario registrado");
-      })
-      .catch((error) => {
-          console.log("Error de firebase > " + error.code);
-          console.log("Error de firebase, mensaje > " + error.message);
-      });
-}
-/* -----Fin register----- */
-
-
-
-
-// inicializacion de firebase
-firebase.initializeApp({
+ // inicializacion de firebase
+ firebase.initializeApp({
   apiKey: "AIzaSyA9KOG2Mc1NSFQ7h9OPNJbNJEDxJ62ivYI",
   authDomain: "red-social-cfa45.firebaseapp.com",
   projectId: "red-social-cfa45"
@@ -117,7 +13,7 @@ var db = firebase.firestore();
 
 // Agregar documento
 function guardar() {
-  let texto = document.getElementById('text').value;
+  var texto = document.getElementById('text').value;
   let name = "mi@mail.com";
   let d = new Date();
   let time = d.getHours() + ":" + d.getMinutes();
@@ -134,7 +30,9 @@ function guardar() {
     .then(function (docRef) {
       console.log("Document written with ID: ", docRef.id);
       console.log("text");
+      console.log('sí se ha guardado');
       document.getElementById('text').value = '';
+      document.getElementById('titulo').value = '';
     })
     .catch(function (error) {
       console.error("Error adding document: ", error);
@@ -152,12 +50,13 @@ db.collection("post").onSnapshot((querySnapshot) => {
       <div id="post">
       <div class="card" style="width: 20rem;">
         <div class="card-body">
-          <h5>${doc.data().title}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${doc.data().name}</h6>
+          <h2>${doc.data().title}</h2>
+          <h11 class="card-subtitle mb-2 text-muted">${doc.data().name}</h11>
           <p>${doc.data().texto}</p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
+          <button class="btn btn-danger" onclick="myFunction(event, '${doc.id}')">Eliminar</button>
+          <button class="btn btn-warning" onclick="editar('${doc.id}', '${doc.data().title}', '${doc.data().texto}')">Editar</button>
+          <h6>${doc.data().time}</h6>
+          </div>
       </div>`
       /* <tr>
             <th scope="row">${doc.data().time}</th>
@@ -170,3 +69,51 @@ db.collection("post").onSnapshot((querySnapshot) => {
 
     });
 });
+
+// Borrar documentos
+function eliminar(id){
+  db.collection("post").doc(id).delete().then(function() {
+    console.log("Document successfully deleted!");
+  }).catch(function(error) {
+    console.error("Error removing document: ", error);
+  });
+}
+
+function myFunction(event, id) {
+  if (confirm("estas seguro??")) {
+    eliminar(id);
+  } else {
+    console.log('no eliminar');
+  }
+}
+
+
+// Editar documento
+function editar(id, titulo, texto){
+  document.getElementById('titulo').value = titulo;
+  document.getElementById('text').value = texto;
+  var boton = document.getElementById('boton');
+  boton.innerHTML = 'Editar';
+  boton.onclick = function(){
+
+
+    let editarPost = db.collection("post").doc(id);
+    let title = document.getElementById('titulo').value;
+    let texto = document.getElementById('text').value;
+
+    return editarPost.update({
+      title: title,
+      texto: texto
+    })
+    .then(function() {
+      console.log("Document successfully updated!");
+      boton.innerHTML = 'Guardar';
+      document.getElementById('titulo').value = '';
+      document.getElementById('text').value = '';
+    })
+    .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+    });
+  }
+  } 
